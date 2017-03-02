167fa86563d2b11b7ae43416005453f39501c3d1
Andrew Mackay <2192793m@student.gla.ac.uk>
2017-03-02 16:47:55 +0000

    Added dummy views

--- SandwichClub/SandwichClub_app/views.py
+++ SandwichClub/SandwichClub_app/views.py
@@ -3,5 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def test(request):
-    return HttpResponse("Test webpage")
+def index(request):
+    return HttpResponse("Home page")
+
+def login(request):
+	return HttpResponse("Login page")
+	
+def profile(request):
+	return HttpResponse("Profile page")
+	
+def register(request):
+	return HttpResponse("Registration page")
+	
+def categories(request):
+	return HttpResponse("Categories page")
+	
+def about(request):
+	return HttpResponse("About page")
