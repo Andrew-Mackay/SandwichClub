54f4e289316849bc79ebbe0946daa3536c599bdc
Miles <milesjhl28@gmail.com>
2017-03-09 16:28:03 +0000

    Added templates directory. Updated settings.py

--- SandwichClub/SandwichClub_app/views.py
+++ SandwichClub/SandwichClub_app/views.py
@@ -4,19 +4,21 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Home page")
+    context_dict = {} #what context is needed?
+    response = render(request, 'index.html', context=context_dict)
+    return response
 
 def login(request):
 	return HttpResponse("Login page")
-	
+
 def profile(request):
 	return HttpResponse("Profile page")
-	
+
 def register(request):
 	return HttpResponse("Registration page")
-	
+
 def categories(request):
 	return HttpResponse("Categories page")
-	
+
 def about(request):
 	return HttpResponse("About page")
