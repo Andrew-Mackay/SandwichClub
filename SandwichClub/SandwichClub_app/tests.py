271384b5ff112aea851a6d1d2fa83516965b8013
Andrew Mackay <2192793m@student.gla.ac.uk>
2017-03-21 17:30:49 +0000

    slight changes to population script and started tests

--- SandwichClub/SandwichClub_app/tests.py
+++ SandwichClub/SandwichClub_app/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
-
+from SandwichClub_app.models import UserProfile, Sandwich
 # Create your tests here.
+
+print("hello1")
+class ModelTests(TestCase):
+    print("hello2")
+    def test_testing(self):
+        print("Hello3")
+
+    def setUp(self):
+        print("Hello4")
+        try:
+            from populate_sando import populate
+            print("hello5")
+            populate()
+            print("hello6")
+        except ImportError:
+            print('The module populate_sando does not exist')
+        except NameError:
+            print('The function populate() does not exist or is not correct')
+        except:
+            print('Something went wrong in the populate() function')
+	
+	
+	
+	def get_user_profile(self, name):
+		from SandwichClub_app.models import UserProfile
+		try:                  
+			user = UserProfile.objects.get(name=name)
+		except UserProfile.DoesNotExist:    
+			user = None
+		return user	
+
+
+
+	def get_sandwich(self, name):
+		from SandwichClub_app.models import Sandwich
+		try:                  
+			sandwich = Sandwich.objects.get(name=name)
+		except Sandwich.DoesNotExist:    
+			sandwich = None
+		return sandwich
+
+
+	def test_SandwichGuru_user_added(self):
+		user = self.get_user_profile('SandwichGuru')
+		self.assertIsNotNone(user)
--- sandwichclub/populate_sando.py
+++ sandwichclub/populate_sando.py
@@ -25,44 +25,34 @@ def populate():
 	sandwiches=[
 	{"title" : "Triple-decker steak sandwich",
 	"description":"The king of sandwiches, this meaty skyscraper is a Ramsay gastro classic, perfect with a glass of peppery Pinotage",
-	"recipe":"bread,mayonnaise,garlic,lettuce,beef,onion,beef,tomato,beef,bread",
-	"maker":"SandwichGuru"},
+	"recipe":"bread,mayonnaise,garlic,lettuce,beef,onion,beef,tomato,beef,bread"},
 	{"title" : "Salt beef club",
 	"description":"An indulgent solo supper of a layered sandwich with sliced beef and creamy dressing ",
-	"recipe":"bread,oliveoil,mayonnaise,mustard,chive,avocado,lettuce,beef,bread",
-	"maker":"SandwichMasta"},
+	"recipe":"bread,oliveoil,mayonnaise,mustard,chive,avocado,lettuce,beef,bread"},
 	{"title" : "BLT - Bacon Lettuce Tomato",
 	"description":"Nothing fancy, just delicious - this is the definitive recipe for a no-nonsense Bacon, Lettuce and Tomato sandwich.",
-	"recipe":"bread,lettuce,tomato,bacon,mayonnaise,bread",
-	"maker":"SandwichGuru"},
+	"recipe":"bread,lettuce,tomato,bacon,mayonnaise,bread"},
 	{"title" : "Best-ever crab sandwich",
 	"description":"A classic crab sandwich made a bit more substantial with a few of Barney's favourite flavours",
-	"recipe":"bread,mayonnaise,ketchup,mustard,pepper,crab,bread",
-	"maker":"SandwichMasta"},
+	"recipe":"bread,mayonnaise,ketchup,mustard,pepper,crab,bread"},
 	{"title" : "Green club sandwich",
 	"description":"This healthy sandwich is packed full of goodness to keep you going until dinner",
-	"recipe":"bread,houmous,avocado,tomato,bread",
-	"maker":"TheBritishSandwichAssociation"},
+	"recipe":"bread,houmous,avocado,tomato,bread"},
 	{"title" : "Camembert sandwich",
 	"description":"A cheese sandwich that is anything but ordinary - oozing with camembert and topped with cranberry sauce to give it a Christmas kick",
-	"recipe":"bread,brie,cranberrysauce,vinegar,butter,bread",
-	"maker":"SandwichMania"},
+	"recipe":"bread,brie,cranberrysauce,vinegar,butter,bread"},
 	{"title" : "Roast Pork and Pickled Cucumber Sandwich",
 	"description":"Make quick pickles by combining cucumber, vinegar, oil, brown sugar, salt, and pepper; they give the sandwich an extra zing",
-	"recipe":"bread,pickle,porkloin,pepper,mayonnaise,bread",
-	"maker":"TheWeeSandwichClub"},
+	"recipe":"bread,pickle,porkloin,pepper,mayonnaise,bread"},
 	{"title" : "Open prawn cocktail sandwich",
 	"description":"A lighter version of the classic prawn cocktail, treat yourself to this satisfying yet low-fat lunch",
-	"recipe":"bread,prawn,cucumber,cherrytomato,lemon,dill,ketchup,mayonnaise,bread",
-	"maker":"TheWeeSandwichClub"},
+	"recipe":"bread,prawn,cucumber,cherrytomato,lemon,dill,ketchup,mayonnaise,bread"},
 	{"title" : "Club sandwich",
 	"description":"This layered sandwich, a favourite of gastro-pubs across the country, makes a tasty meal for one",
-	"recipe":"bread,bacon,chicken,salad,boiledegg,tomato,mayonnaise,bread",
-	"maker":"TheBritishSandwichAssociation"},
+	"recipe":"bread,bacon,chicken,salad,boiledegg,tomato,mayonnaise,bread"},
 	{"title" : "Turkey & bacon club",
 	"description":"Satisfy your hunger with this tasty and hearty club sandwich, perfect for weekend breakfast",
-	"recipe":"bread,bacon,turkey,butter,mayonnaise,mustard,lettuce,avocado,bread",
-	"maker":"SandwichMania"},]
+	"recipe":"bread,bacon,turkey,butter,mayonnaise,mustard,lettuce,avocado,bread"},]
 	
 	
 	users2 = []
@@ -70,8 +60,9 @@ def populate():
 		users2.append(add_user(usar["username"],usar["website"]))
 	
 	for sando in sandwiches:
-		i = random.randint(0, len(users2))
-		add_sando(sando['title'],sando['description'],sando['recipe'], users2[i])
+		i = random.randint(0, len(users2)-1)
+		r = random.randint(0,5)
+		add_sando(sando['title'],sando['description'],sando['recipe'], users2[i], r)
 		
 	for uzar in UserProfile.objects.all():
 		print("- {0}".format(str(uzar)))
@@ -79,12 +70,12 @@ def populate():
 	for sandowitch in Sandwich.objects.all():
 		print("- {0}".format(str(sandowitch)))
 		
-def add_sando(title,description,recipe,maker):
+def add_sando(title,description,recipe,maker,rating):
 	print(title, description, recipe, maker)
 	s = Sandwich.objects.get_or_create(title = title, maker = maker)[0]
 	s.description = description
 	s.recipe = recipe
-	#s.maker = maker
+	s.rating = rating
 	s.save()
 	return s
 	
