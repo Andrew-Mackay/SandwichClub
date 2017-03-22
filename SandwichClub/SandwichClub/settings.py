3df9a47dfd94b1b74e98d781f81b5191fda73576
Andrew Mackay <2192793m@student.gla.ac.uk>
2017-03-22 15:23:18 +0000

    password change feature improved

--- SandwichClub/SandwichClub/settings.py
+++ SandwichClub/SandwichClub/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'SandwichClub_app',
-	'registration', # add in the registration package
+    'registration', # add in the registration package
 ]
 
 MIDDLEWARE = [
