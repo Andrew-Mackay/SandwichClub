aa8d77eb7e29d01491b7abdfeeb4f7eec5569de4
Miles <milesjhl28@gmail.com>
2017-03-22 16:08:33 +0000

    Edited sandwich page html/css and population script

--- SandwichClub/static/js/sandwich.js
+++ SandwichClub/static/js/sandwich.js
@@ -1,39 +1,39 @@
-var ing = {}; //ingredients
-// format: ing.<CODE> = <Verbose Name>
-ing.br = "Bread";
-ing.bc = "Bacon";
-ing.lt = "Lettuce";
-ing.tm = "Tomato";
-ing.hm = "Ham";
-ing.rb = "Roast Beef";
-ing.av = "Avocado";
-
-window.onload = function(){
-    //displays the sandwich's recipe
-    var recipe = document.getElementById("recipe").innerHTML.split(",");
-    for(var i=0;i<recipe.length;i++){
-        makeIngredient(recipe[i]);
-    }
-
-    // displays the sandwich's rating
-    rating_span = document.getElementById("rating");
-    rating = Number(rating_span.innerHTML);
-    rating_span.innerHTML = "(";
-    for(var i=0;i<rating;i++){ rating_span.innerHTML += "★"; }
-    for(var i=0;i<5-rating;i++){ rating_span.innerHTML += "☆"; }
-    rating_span.innerHTML += ")";
-
-}
+"use strict";
 
 // Displays an ingredient tile with the given code
-makeIngredient = function(code){
+var makeIngredient = function(ingredient){
     var block = document.createElement("div");
     block.className = "ingredient";
 
     var img = document.createElement("img");
-    img.setAttribute("src","../../static/img/"+ing[code]+".jpg");
-    img.setAttribute("alt",ing[code]);
+    img.setAttribute("src","../../static/img/"+slug(ingredient)+".jpg");
+    img.setAttribute("alt",ingredient);
     block.appendChild(img);
 
     document.getElementById("sandwich-recipe").appendChild(block);
-}
+};
+
+//returns a file-friendly name for a string
+var slug = function(str){
+    str = str.toLowerCase();
+    str = str.replace(" ","-");
+    return str;
+};
+
+window.onload = function(){
+    //displays the sandwich's recipe
+    var recipe = document.getElementById("recipe").innerHTML.split(",");
+    var i;
+    for(i=0;i<recipe.length;i+=1){
+        makeIngredient(recipe[i]);
+    }
+
+    // displays the sandwich's rating
+    var rating_span = document.getElementById("rating");
+    var rating = Number(rating_span.innerHTML);
+    rating_span.innerHTML = "(";
+    for(i=0;i<rating;i+=1){ rating_span.innerHTML += "★"; }
+    for(i=0;i<5-rating;i+=1){ rating_span.innerHTML += "☆"; }
+    rating_span.innerHTML += ")";
+
+};
