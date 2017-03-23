670b8c18cb8ed03b54f7e687694a050f59b8cb95
Miles <milesjhl28@gmail.com>
2017-03-23 12:59:33 +0000

    Redid the create_sandwich page so it's prettier

--- SandwichClub/static/js/sandwich.js
+++ SandwichClub/static/js/sandwich.js
@@ -1,5 +1,12 @@
 "use strict";
 
+//returns a file-friendly name for a string
+var slug = function(str){
+    str = str.toLowerCase();
+    str = str.replace(" ","-");
+    return str;
+};
+
 // Displays an ingredient tile with the given code
 var makeIngredient = function(ingredient){
     var block = document.createElement("div");
@@ -11,27 +18,46 @@ var makeIngredient = function(ingredient){
     document.getElementById("sandwich-recipe").appendChild(block);
 };
 
-//returns a file-friendly name for a string
-var slug = function(str){
-    str = str.toLowerCase();
-    str = str.replace(" ","-");
-    return str;
+//cleans up user input strings
+var cleanstr = function(str){
+    str = str.trim();           //removes whitespace from either side
+    str = str.toLowerCase();    //normalizes capitalization
+    str = str.replace(/\s{2,}/g, ' '); //removes double spaces
+    var i;
+    for(i=0;i<str.length;i+=1){ //capitalizes each word
+        if(i===0 || str.charAt(i-1)===' '){
+            str = str.substr(0,i) + str.charAt(i).toUpperCase() + str.substr(i+1);
+        }
+    }
+};
+
+//adds an ingredient (for use in create_sandwich.html)
+var addIng = function(){
+    var ingredient = document.getElementById("recipe_input_box").value;
+    document.getElementById("recipe_input_box").value = "";
+    makeIngredient(ingredient);
+    var rec = document.getElementById("id_recipe");
+    rec.value = (rec.value + ingredient + ',');
 };
 
 window.onload = function(){
-    //displays the sandwich's recipe
-    var recipe = document.getElementById("recipe").innerHTML.split(",");
     var i;
-    for(i=0;i<recipe.length;i+=1){
-        makeIngredient(recipe[i]);
+    if(document.getElementById("sandwich-recipe").getAttribute("create")==="true"){
+        //set up create sandwich recipe
+        document.getElementById("ing_submit").addEventListener("click",addIng);
+    } else{
+        //displays the sandwich's recipe
+        var recipe = document.getElementById("recipe").innerHTML.split(",");
+        for(i=0;i<recipe.length;i+=1){
+            makeIngredient(recipe[i]);
+        }
+
+        // displays the sandwich's rating
+        var rating_span = document.getElementById("rating");
+        var rating = Number(rating_span.innerHTML);
+        rating_span.innerHTML = "(";
+        for(i=0;i<rating;i+=1){ rating_span.innerHTML += "★"; }
+        for(i=0;i<5-rating;i+=1){ rating_span.innerHTML += "☆"; }
+        rating_span.innerHTML += ")";
     }
-
-    // displays the sandwich's rating
-    var rating_span = document.getElementById("rating");
-    var rating = Number(rating_span.innerHTML);
-    rating_span.innerHTML = "(";
-    for(i=0;i<rating;i+=1){ rating_span.innerHTML += "★"; }
-    for(i=0;i<5-rating;i+=1){ rating_span.innerHTML += "☆"; }
-    rating_span.innerHTML += ")";
-
 };
