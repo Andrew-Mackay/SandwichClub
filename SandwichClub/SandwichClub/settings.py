a6013b77fc3f16cc7df7d9ec4eb1123bfe239ea5
Miles <milesjhl28@gmail.com>
2017-03-23 13:06:08 +0000

    Added back lost changes in settings.py

--- SandwichClub/SandwichClub/settings.py
+++ SandwichClub/SandwichClub/settings.py
@@ -30,9 +30,9 @@ MEDIA_URL ='/media/'
 SECRET_KEY = 'j938#qk_0$wzu49=5b$w#9vnu-86klk05=k^p)*#$ic2j$r-&d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['sandwichclub.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['sandwichclub.pythonanywhere.com', 'localhost']
 
 
 # Application definition
